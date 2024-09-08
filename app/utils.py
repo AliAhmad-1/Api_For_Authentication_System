c5b03ff7cd7b0808e8059e61c360aa96b5561c14
Maamoun Haj Najeeb <87011464+maamounhajnajeeb@users.noreply.github.com>
2024-09-08 18:48:30 +0300

    Update utils.py
    updating

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,15 @@
 from django.core.mail import EmailMessage
 
 class Util:
+    
     @staticmethod
     def send_email(data):
         email=EmailMessage(
-        subject=data['subject'],
-        body=data['body'],
-        from_email='eng.alihasanahmad@gmail.com',
-        to=(data['to_email'],)
+            subject=data['subject'],
+            body=data['body'],
+            from_email='eng.alihasanahmad@gmail.com',
+            to=(data['to_email'],)
         )
         email.send()
+
+        # note: handle email error
